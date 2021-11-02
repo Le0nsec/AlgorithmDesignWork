7179547946373aa559403487646ab4dfe4fa4b21
Le0nsec <leon.clqorz@gmail.com>
2021-11-02 10:58:56 +0800

    edit: bigNat.go

--- BigintegerCalculator/big/bigNat.go
+++ BigintegerCalculator/big/bigNat.go
@@ -5,25 +5,10 @@ import (
 	"sync"
 )
 
-// An unsigned integer x of the form
-//
-//   x = x[n-1]*_B^(n-1) + x[n-2]*_B^(n-2) + ... + x[1]*_B + x[0]
-//
-// with 0 <= x[i] < _B and 0 <= i < n is stored in a slice of length n,
-// with the digits x[i] as the slice elements.
-//
-// A number is normalized if the slice contains no leading 0 digits.
-// During arithmetic operations, denormalized values may occur but are
-// always normalized before returning the final result. The normalized
-// representation of 0 is the empty or nil slice (length = 0).
-//
 type nat []Word
 
 var (
 	natOne = nat{1}
-	// natTwo  = nat{2}
-	// natFive = nat{5}
-	// natTen  = nat{10}
 )
 
 func (z nat) clear() {
